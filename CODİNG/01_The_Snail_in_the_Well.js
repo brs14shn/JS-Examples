1766f49cea9f3c5e79da309a6b8a7cc3380a2feb
brs14shn <brs>
2022-04-25 16:58:06 +0300

    first commit

---
+++ CODİNG/01_The_Snail_in_the_Well.js
@@ -0,0 +1,19 @@
+/* The snail climbs up 7 feet each day and slips back 2 feet each night.
+How many days will it take the snail to get out of a well with the given depth?*/
+
+var depth=31
+var day = 0;
+    var total = 0;
+    while(total<depth){
+        day = day + 1;
+        total = total + 7;
+        if(total >= depth){
+            console.log(day);
+            break;
+        }
+        total = total - 2;
+    }
+console.log(day)
+
+
+
