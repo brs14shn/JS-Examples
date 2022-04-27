c459608e478405cbd86953a747f0955b8822a397
brs14shn <brs>
2022-04-27 12:52:57 +0300

    calculator file was added.

---
+++ CODİNG/02_Calculator.js
@@ -0,0 +1,19 @@
+
+
+
+
+function maths(a,b,opt){
+    if(opt=="+"){
+        console.log(a+b);
+    }
+    else if(opt=="-"){
+        console.log(a-b);
+    }
+    else if(opt=="*"){
+        console.log(a*b);
+    }
+    else if(opt=="/"){
+        console.log(a/b);
+    }
+}
+maths(a,b,"+");
