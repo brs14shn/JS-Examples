5ab04af96098465f698a4a592a0d35977a4269e8
brs14shn <brs>
2022-04-28 23:21:35 +0300

    leap year file was added

---
+++ CODİNG/03_Leap_Year.js
@@ -0,0 +1,30 @@
+//Leap year
+const year=new Date().getFullYear();
+const month=new Date().getMonth();
+
+let dayCount;
+switch (month) {
+  case 0:
+  case 2:
+  case 4:
+  case 6:
+  case 7:
+  case 9:
+  case 11:
+    dayCount = 31;
+    break;
+  case 3:
+  case 5:
+  case 8:
+  case 10:
+    dayCount = 30;
+    break;
+  case 1:
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) dayCount = 29; //HİNT:case (===) işlemi yapar.İf (==) işlemi yapar
+    else dayCount = 28;
+    break;
+  default:
+    dayCount = -1; // invalid month
+}
+
+console.log(dayCount); // 29
