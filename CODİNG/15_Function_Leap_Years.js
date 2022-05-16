e3475f56e3c2f0e951934585c8cd86a2609d6d50
brs14shn <brs>
2022-05-16 10:42:48 +0300

    file was updated.

--- CODİNG/15_Function_Leap_Years.js
+++ CODİNG/15_Function_Leap_Years.js
@@ -6,7 +6,7 @@
 
 //! FUNCTİON DECLARATİON
 
-function LeapYear(year){
+function LeapYear(){
     if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
         console.log(`${year} is a leap year `);
     }else{
