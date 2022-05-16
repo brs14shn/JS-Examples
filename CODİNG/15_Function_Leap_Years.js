ee309926dbd4eeb566881616625ab68fb7628290
brs14shn <brs>
2022-05-16 08:36:23 +0300

    leap_year file was added

---
+++ CODİNG/15_Function_Leap_Years.js
@@ -0,0 +1,44 @@
+
+
+//leap year : ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+
+
+
+//! FUNCTİON DECLARATİON
+
+function LeapYear(year){
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+        console.log(`${year} is a leap year `);
+    }else{
+        console.log(`${year} is not a leap year `);
+    }
+}
+let year=parseInt(prompt("Enter a year"));
+LeapYear()
+
+
+//! FUNCTİON EXPRESSİONS
+
+let LeapYearone=function(){
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+        console.log(`${year} is a leap year `);
+    }else{
+        console.log(`${year} is not a leap year `);
+    }
+}
+let yearone=parseInt(prompt("Enter a year"));
+LeapYearone()
+
+//! FUNCTİON ARROW
+
+let=yeartwo=parseInt(prompt("Enter a year"));
+
+const LeapYeartwo=()=>
+{   
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+    return `${year} is a leap year `;
+    }else{
+    return `${year} is not a leap year `;
+    }
+}
+console.log(LeapYear());
