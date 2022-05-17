7716a2313182cc91e0c20340c57f2e38835132c9
brs14shn <brsshn292929@gmail.com>
2022-05-17 18:15:41 +0300

    array filters file was added

---
+++ code/18_Array_Filter.js
@@ -0,0 +1,17 @@
+
+
+
+//! Description:
+
+/*JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:*/
+
+
+//Examples :getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+
+function getEvenNumbers(numbersArray){
+    let x= numbersArray.filter((y) => y%2 == 0)
+    return x
+}
