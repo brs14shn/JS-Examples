0fdd1d50607e1fc4d3acf188043ac1f375f26080
brs14shn <brsshn292929@gmail.com>
2022-05-20 17:47:34 +0300

    create phone number was added

---
+++ code/21_Create_Phone_Number.js
@@ -0,0 +1,30 @@
+
+
+/*Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.*/
+
+//createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+
+/*function createPhoneNumber(numbers){
+    return `(${numbers.slice(0,3).join("")}) ${numbers.slice(3,6).join("")}-${numbers.slice(6).join("")}`
+  }*/
+
+
+  /*function createPhoneNumber(numbers){
+    return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
+  }
+  console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))*/
+
+  
+let phonenumber=prompt("Enter a number")
+  function createPhoneNumber(numbers){
+    var format = "(xxx) xxx-xxxx";
+    
+    for(var i = 0; i < numbers.length; i++)
+    {
+      format = format.replace('x', numbers[i]);
+    }
+    
+    return format
+  }
+console.log(createPhoneNumber(phonenumber);
