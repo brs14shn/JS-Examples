8aeb9ad119eae03b57263a5d3d941e31b72f7597
brs14shn <brsshn292929@gmail.com>
2022-05-25 14:07:15 +0300

    updated

---
+++ code/26_Finding_Missing_Number.js
@@ -0,0 +1,37 @@
+
+
+
+//Description:
+/*The following was a question that I received during a technical interview for an entry level software developer position. 
+I thought I'd post it here so that everyone could give it a go:
+
+You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?*/
+
+ let arr1 = [45, 53, 10, 100, 30, 85, 72, 69, 93, 98, 27, 73, 82, 91, 60, 5, 79, 88, 18, 71, 36, 44, 22, 89, 40, 59, 80, 81, 67, 25, 54, 13, 64, 56, 39, 48, 92, 84, 94, 87, 90, 77, 63, 32, 68, 37, 96, 23, 0, 95, 1, 52, 78, 6, 57, 50, 2, 46, 19, 76, 47, 14, 4, 3, 29, 17, 11, 21, 24, 74, 65, 12, 83, 28, 41, 66, 7, 58, 55, 51, 43, 97, 42, 86, 49, 31, 20, 75, 70, 34, 33, 38, 8, 15, 62, 35, 61, 99, 16]
+ 
+
+//SOLUTİON-1
+function missNumber(nums){
+    let arr2=nums.sort(function(a, b){return a-b});
+    for(let i =0;i<arr2.length;i++){
+    if(nums[i]!==i){ 
+    return (i)}
+ }
+}
+console.log(missNumber(arr1))
+
+//SOLUTİON-2
+function missingNo(nums) {
+  return 5050 - nums.reduce((a, b) => a + b, 0)
+}
+console.log(missingNo(arr1))
+
+//SOLUTİON-3
+function missingNo (numbers) {
+  let result = 5050
+
+  for (let number of numbers) {
+    result -= number
+  }
+  return result
+}
