88cd47c12bd29b12e7ed6ae78627bb72be4947fc
brs14shn <brsshn292929@gmail.com>
2022-08-18 23:48:28 +0300

    Vovel Remover was added

---
+++ code/31_Vovel_Remover.js
@@ -0,0 +1,13 @@
+
+//? DESCRİPTİON:
+
+//Create a function called shortcut to 
+//remove the lowercase vowels (a, e, i, o, u ) 
+//in a given string.
+
+
+const VovelRemover=(str)=>{
+    return str.split("").map((el)=>(el.replace(/[aeiou]/g, '' ))).join("")
+}
+
+console.log(VovelRemover("Erhan"));
