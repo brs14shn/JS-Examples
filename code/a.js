f06f9ff55fc9b70a6ba2bb0a2c611e817363a9d0
brs14shn <brsshn292929@gmail.com>
2022-09-12 00:00:37 +0300

    updated

--- code/a.js
+++ code/a.js
@@ -5,7 +5,9 @@
 function findLongestWord(str) {
     const splitText=str.split(" ").sort()
     const longestWord=splitText.sort(function(a, b) { return b.length - a.length; }); 
-    return longestWord[0]
+    //! [ 'jumped','brown','quick','lazy','over','The','dog','fox','the' ]
+    //! [ 'Turkish', 'breed', 'cats', 'like', 'Van', 'the', 'of', 'I' ]
+    return longestWord
    
    
   }
@@ -13,3 +15,14 @@ function findLongestWord(str) {
   console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // jumped
   console.log(findLongestWord("I like cats of the Turkish Van breed")); // Turkish
  
+
+  //* ==============REDUCE=====================
+
+  function findLongestWord(str) {
+     let longestWord = str.split(' ')
+     .reduce(function(longest, currentWord) 
+     { return currentWord.length > longest.length ? currentWord : longest; }, ""); 
+     return longestWord; } 
+     
+     
+    console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
