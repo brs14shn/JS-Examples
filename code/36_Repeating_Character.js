5d346dcdac26bd8b55f82ea8d5be76f4cc750080
brs14shn <brsshn292929@gmail.com>
2022-09-19 01:03:43 +0300

    repeat_character

---
+++ code/36_Repeating_Character.js
@@ -0,0 +1,24 @@
+
+let inputString="hardcorecoder";
+
+let length=inputString.length
+
+let repeat={}
+
+
+for(let i =0;i<length;i++){
+    let ch=inputString[i]
+    repeat[ch] =repeat[ch] ? repeat[ch]+1 :1
+    console.log(repeat);
+}
+
+let keys=Object.keys(repeat)
+console.log(keys);
+
+for (let i of keys){
+    console.log(i);
+    if(repeat[i]>=2){
+        console.log(i);
+        return
+    }
+}
